c843a48052e8d261ca816e1a130ba3cf950207d8
Amjath-s <amjaths788@gmail.com>
2025-10-11 01:50:46 +0530

    cors setup

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,11 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://portfolio-six-sage-35.vercel.app/",
+  methods: ["POST", "GET", "OPTIONS"],
+  credentials: true
+}));
 
 app.post("/api/contact", async (req, res) => {
   const { name, email, message } = req.body;
